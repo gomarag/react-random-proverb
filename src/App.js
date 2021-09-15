49bf567fc8d04f33e1972ff7e5bdc39c534bfd0e
gomarag <maragriver@gmail.com>
2021-09-15 12:30:35 +0900

    add: 랜덤하게 문구 출력

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const proverbs = ["책임happy", "도덕적hey", "자연의 each","alone장악", "매관magic", "charm과 거짓", "혹세moomin", "부정buffet"];
+
+  const getRandomIndex = (length) => {
+    return parseInt(Math.random() * length);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {proverbs[getRandomIndex(proverbs.length)]}
       </header>
     </div>
   );
